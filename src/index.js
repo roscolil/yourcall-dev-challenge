0cd620b2df3ccd2b839fb678d05f6681d4ba4e55
Ross <ross.lillis@gmail.com>
2021-03-23 07:06:20 +1100

    feat: add apollo client wrapper

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
 import './index';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://graphql-weather-api.herokuapp.com/',
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <ApolloHooksProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ApolloHooksProvider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
